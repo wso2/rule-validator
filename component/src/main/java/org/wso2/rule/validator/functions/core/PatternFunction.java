32b9764aceaf63fa5701fe3b66b1565123cea81b
Oshanath Rajawasam <oshanath@wso2.com>
2025-01-30 14:09:07 +0530

    Fixed the regex tests of Pattern function.

--- component/src/main/java/org/wso2/rule/validator/functions/core/PatternFunction.java
+++ component/src/main/java/org/wso2/rule/validator/functions/core/PatternFunction.java
@@ -25,6 +25,7 @@ import org.wso2.rule.validator.functions.LintFunction;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
@@ -36,6 +37,16 @@ public class PatternFunction extends LintFunction {
 
     public PatternFunction(Map<String, Object> options) {
         super(options);
+    }
+
+    private static class PatternAndFlags {
+        public String pattern;
+        public String flags;
+
+        public PatternAndFlags(String pattern, String flags) {
+            this.pattern = pattern;
+            this.flags = flags;
+        }
     }
 
     @Override
@@ -50,11 +61,6 @@ public class PatternFunction extends LintFunction {
         if (!options.containsKey(Constants.RULESET_PATTERN_MATCH) &&
                 !options.containsKey(Constants.RULESET_PATTERN_NOT_MATCH)) {
             errors.add("Pattern function requires either match or notMatch options");
-        }
-
-        if (options.containsKey(Constants.RULESET_PATTERN_MATCH) &&
-                options.containsKey(Constants.RULESET_PATTERN_NOT_MATCH)) {
-            errors.add("Pattern function cannot contain both match and notMatch options.");
         }
 
         if (options.containsKey(Constants.RULESET_PATTERN_MATCH) &&
@@ -77,6 +83,22 @@ public class PatternFunction extends LintFunction {
             errors.add("Pattern function notMatch option is not a valid regex pattern.");
         }
 
+        if (options.containsKey(Constants.RULESET_PATTERN_MATCH)) {
+            PatternAndFlags patternAndFlags = extractPatternAndFlags(
+                    (String) options.get(Constants.RULESET_PATTERN_MATCH));
+            if (getFlagsFromFlagString(patternAndFlags.flags) == -1) {
+                errors.add("Pattern function match option contains invalid flags.");
+            }
+        }
+
+        if (options.containsKey(Constants.RULESET_PATTERN_NOT_MATCH)) {
+            PatternAndFlags patternAndFlags = extractPatternAndFlags(
+                    (String) options.get(Constants.RULESET_PATTERN_NOT_MATCH));
+            if (getFlagsFromFlagString(patternAndFlags.flags) == -1) {
+                errors.add("Pattern function notMatch option contains invalid flags.");
+            }
+        }
+
         return errors;
     }
 
@@ -87,6 +109,56 @@ public class PatternFunction extends LintFunction {
         } catch (PatternSyntaxException e) {
             return false;
         }
+    }
+
+    private PatternAndFlags extractPatternAndFlags(String regex) {
+        String pattern = regex;
+        String flags = "";
+        int lastSlash = regex.lastIndexOf("/");
+        if (lastSlash != -1) {
+            pattern = regex.substring(1, lastSlash);
+            flags = regex.substring(lastSlash + 1);
+        }
+        return new PatternAndFlags(pattern, flags);
+    }
+
+    private int getFlagsFromFlagString(String flags) {
+        int flag = 0;
+
+        if (flags.trim().isEmpty()) {
+            return flag;
+        }
+
+        if (flags.contains("i")) {
+            flag |= Pattern.CASE_INSENSITIVE;
+        }
+        if (flags.contains("u")) {
+            flag |= Pattern.UNICODE_CASE;
+        }
+        if (flags.contains("m")) {
+            flag |= Pattern.MULTILINE;
+        }
+        if (flags.contains("s")) {
+            flag |= Pattern.DOTALL;
+        }
+        if (flags.contains("d")) {
+            flag |= Pattern.UNIX_LINES;
+        }
+
+        if (!flags.matches("[iumsdgy]+")) {
+            return -1;
+        }
+
+        return flag;
+    }
+
+    private boolean matches(String value, PatternAndFlags patternAndFlags) {
+
+        int flags = getFlagsFromFlagString(patternAndFlags.flags);
+
+        Pattern pattern = Pattern.compile(patternAndFlags.pattern, flags);
+        Matcher matcher = pattern.matcher(value);
+        return matcher.matches();
     }
 
     public boolean executeFunction(LintTarget target) {
@@ -100,14 +172,27 @@ public class PatternFunction extends LintFunction {
             return true;
         }
 
+        boolean matchResult = false;
+        boolean notMatchResult = false;
+
         try {
             if (match != null) {
-                return target.value.toString().matches((String) match);
-            } else if (notMatch != null) {
-                return !target.value.toString().matches((String) notMatch);
-            } else {
-                throw new RuntimeException("Pattern function requires either match or notMatch options");
+                PatternAndFlags matchPatternAndFlags = extractPatternAndFlags((String) match);
+                boolean result = matches((String) target.value, matchPatternAndFlags);
+                if (notMatch == null) {
+                    return result;
+                }
+                matchResult = result;
             }
+            if (notMatch != null) {
+                PatternAndFlags notMatchPatternAndFlags = extractPatternAndFlags((String) notMatch);
+                boolean result = !matches((String) target.value, notMatchPatternAndFlags);
+                if (match == null) {
+                    return result;
+                }
+                notMatchResult = result;
+            }
+            return matchResult && notMatchResult;
         } catch (PatternSyntaxException e) {
             return false;
         }
