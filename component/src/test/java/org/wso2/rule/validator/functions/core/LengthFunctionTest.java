16fcdd4d5fbbd00780082d999e5b78a32aab4657
Ashan Rathnaweera <ashanahr@gmail.com>
2025-02-19 14:44:45 +0530

    Add license header

--- component/src/test/java/org/wso2/rule/validator/functions/core/LengthFunctionTest.java
+++ component/src/test/java/org/wso2/rule/validator/functions/core/LengthFunctionTest.java
@@ -15,6 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.wso2.rule.validator.functions.core;
 
 import org.junit.jupiter.api.BeforeEach;
